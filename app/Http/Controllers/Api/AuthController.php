e68d2fadd1e991752f52babbe0c45b6ef8936039
miqueiasnasc <miqueiasnascimento987@gmail.com>
2025-10-23 14:46:43 -0300

    auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        // Valida as credenciais da requisição
+        // Valida as credenciais da requi   sição
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
