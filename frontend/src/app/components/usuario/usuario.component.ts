72744598b1dc9c910d4cda97680d626f84ce1b10
Gabriel Porto <mendes.porto@aluno.ifsp.edu.br>
2025-12-03 00:41:06 -0300

    Comentei os users

--- frontend/src/app/components/usuario/usuario.component.ts
+++ frontend/src/app/components/usuario/usuario.component.ts
@@ -1,16 +1,16 @@
-// 1. Importa o 'CommonModule' e o serviço
+// 1. Importa o 'CommonModule'
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'; // Removi 'OnInit' pois não vamos usar agora
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-
 import { Usuario } from '../../models/models';
-import { UsuarioApiService } from '../../services/usuario-api.service';
+// import { UsuarioApiService } from '../../services/usuario-api.service';
 import { BarraLateralComponent } from '../shared/barra-lateral/barra-lateral.component';
 
 @Component({
   selector: 'app-usuario',
+  standalone: true,
   imports: [
     CommonModule,
     MatTableModule,
@@ -20,23 +20,25 @@ import { BarraLateralComponent } from '../shared/barra-lateral/barra-lateral.com
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
-export class UsuarioComponent implements OnInit {
+// REMOVIDO "implements OnInit" para não dar erro pela falta do método ngOnInit
+export class UsuarioComponent {
 
-  usuarios: Usuario[] = [];
-  displayedColumns = ['nome', 'telefone', 'email', 'perfil'];
+  // usuarios: Usuario[] = [];
+  // displayedColumns = ['nome', 'telefone', 'email', 'perfil'];
 
-  constructor(
-    private usuarioApiService: UsuarioApiService
-  ) { }
+  // constructor(
+  //   private usuarioApiService: UsuarioApiService
+  // ) { }
 
+  /*
   ngOnInit(): void {
     this.carregarUsuarios();
   }
+  */
 
-  carregarUsuarios(): void {
+  /* carregarUsuarios(): void {
     this.usuarioApiService.getUsuarios().subscribe({
       next: (data) => {
-
         this.usuarios = data;
         console.log('Dados carregados:', this.usuarios);
       },
@@ -45,4 +47,5 @@ export class UsuarioComponent implements OnInit {
       }
     });
   }
+  */
 }
